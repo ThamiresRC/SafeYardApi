4cd1eb58d28ecb0d1d680b773fc9fd84cbbd767d
ThamiresRC <seu-email-no-github@example.com>
2025-10-31 14:51:46 -0300

    feat: postgres V12 seed users

--- src/main/resources/db/migration-postgres/V12__seed_users_safeyard.sql
+++ src/main/resources/db/migration-postgres/V12__seed_users_safeyard.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (ativo, email, nome, role, senha)
 SELECT 1, 'admin@safeyard.com', 'Administrador SafeYard', 'ADMIN',
-       '$2a$10$uQkXULE42q9BKBFszPN5veYZ2CtXp/eSqZPhJpGJE5CBHAJeX8aUi'
+       '$2a$10$uQkXULE4zq9BKBFSzPN5veY2ZCtkXp/eSqZPhJpGjE5CBHAJeX8aUi'
     WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@safeyard.com');
 
 INSERT INTO users (ativo, email, nome, role, senha)
 SELECT 1, 'func@safeyard.com', 'Funcionário SafeYard', 'FUNCIONARIO',
-       '$2a$10$uQkXULE42q9BKBFszPN5veYZ2CtXp/eSqZPhJpGJE5CBHAJeX8aUi'
+       '$2a$10$uQkXULE4zq9BKBFSzPN5veY2ZCtkXp/eSqZPhJpGjE5CBHAJeX8aUi'
     WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'func@safeyard.com');
 
 INSERT INTO users (ativo, email, nome, role, senha)
 SELECT 1, 'cliente@safeyard.com', 'Cliente SafeYard', 'CLIENTE',
-       '$2a$10$uQkXULE42q9BKBFszPN5veYZ2CtXp/eSqZPhJpGJE5CBHAJeX8aUi'
+       '$2a$10$uQkXULE4zq9BKBFSzPN5veY2ZCtkXp/eSqZPhJpGjE5CBHAJeX8aUi'
     WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'cliente@safeyard.com');
